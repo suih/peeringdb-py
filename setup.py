605685eeb6bf129ac3d1d7263694f7b8640ea15f
Nat Morris <nat@netflix.com>
2015-06-10 17:31:15 -0700

    setup files for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'peeringdb',
+  packages = ['peeringdb'], # this must be the same as the name above
+  version = '0.1',
+  description = 'PeeringDB API Python library',
+  author = 'Nat Morris',
+  author_email = 'nat@netflix.com',
+  url = 'https://github.com/natm/peeringdb-py',
+  download_url = 'https://github.com/natm/peeringdb-py/tarball/0.1',
+  keywords = ['peering', 'peeringdb', 'bgp'],
+  classifiers = [],
+)
